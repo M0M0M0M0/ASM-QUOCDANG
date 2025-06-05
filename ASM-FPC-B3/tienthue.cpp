b37b58833ead27307a4594040c4c5c4b582d399b
Quốc Đặng <108169626+M0M0M0M0@users.noreply.github.com>
2025-06-05 19:20:47 +0700

    Update tienthue.cpp

--- ASM-FPC-B3/tienthue.cpp
+++ ASM-FPC-B3/tienthue.cpp
@@ -12,19 +12,19 @@ int main(){
 	}while(x<0);
 	x= money - 11000000; //x = thu nhap tinh thue
 	int y;
-	if(money>52000000){
+	if(x>52000000){
 		y=(5*5000000/100+10*5000000/100+8000000*15/100+14000000*20/100+20000000*25/100+(x-52000000)*0.25;
 		printf("Tien thue phai tra la %d VND",y);
-	}else if(money>32000000){
+	}else if(x>32000000){
 		y=x*(5*5000000/100+10*5000000/100+8000000*15/100+14000000*20/100+(x-32000000)*0.2;
 		printf("Tien thue phai tra la %d VND",y);	
-	}else if(money>18000000){
+	}else if(x>18000000){
 		y=x*(5*5000000/100+10*5000000/100+8000000*15/100+(x-18000000)*0.15);
 		printf("Tien thue phai tra la %d VND",y);	
-	}else if(money>10000000){
+	}else if(x>10000000){
 		y=x*(5*5000000/100+10*5000000/100+(x-10000000)*0.1);
 		printf("Tien thue phai tra la %d VND",y);
-	}else if(money>5000000){
+	}else if(x>5000000){
 		y=x*(5*5000000/100+(x-5000000)*0.05);
 		printf("Tien thue phai tra la %d VND",y);
 	}else{
