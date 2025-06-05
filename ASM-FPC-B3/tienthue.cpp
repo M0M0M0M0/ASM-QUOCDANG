e5bb6958ef6d831342aa5bacfc460b5326f23893
Quốc Đặng <108169626+M0M0M0M0@users.noreply.github.com>
2025-06-05 19:19:26 +0700

    Update tienthue.cpp

--- ASM-FPC-B3/tienthue.cpp
+++ ASM-FPC-B3/tienthue.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 	int x,money;
-	printf("Nhap tien luong theo nam: ");
+	printf("Nhap tien luong theo thang: ");
 	
 	do{
 		scanf("%d",&money);
@@ -10,22 +10,22 @@ int main(){
 			printf("Vui long nhap so tien luong lon hon 0\n");
 		}
 	}while(x<0);
-	x= money - 11000000;
+	x= money - 11000000; //x = thu nhap tinh thue
 	int y;
-	if(money>384000000){
-		y=x*0.25;
+	if(money>52000000){
+		y=(5*5000000/100+10*5000000/100+8000000*15/100+14000000*20/100+20000000*25/100+(x-52000000)*0.25;
 		printf("Tien thue phai tra la %d VND",y);
-	}else if(money>216000000){
-		y=x*0.2;
+	}else if(money>32000000){
+		y=x*(5*5000000/100+10*5000000/100+8000000*15/100+14000000*20/100+(x-32000000)*0.2;
 		printf("Tien thue phai tra la %d VND",y);	
-	}else if(money>120000000){
-		y=x*0.15;
+	}else if(money>18000000){
+		y=x*(5*5000000/100+10*5000000/100+8000000*15/100+(x-18000000)*0.15);
 		printf("Tien thue phai tra la %d VND",y);	
-	}else if(money>60000000){
-		y=x*0.1;
+	}else if(money>10000000){
+		y=x*(5*5000000/100+10*5000000/100+(x-10000000)*0.1);
 		printf("Tien thue phai tra la %d VND",y);
-	}else if(money>11000000){
-		y=x*0.05;
+	}else if(money>5000000){
+		y=x*(5*5000000/100+(x-5000000)*0.05);
 		printf("Tien thue phai tra la %d VND",y);
 	}else{
 		printf("Khong phai nop thue");
