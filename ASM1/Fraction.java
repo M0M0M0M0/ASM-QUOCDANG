96bf70c6165c9c781e6faa23c6d5ee166ebfb381
Quốc Đặng <108169626+M0M0M0M0@users.noreply.github.com>
2026-01-21 18:46:33 +0700

    Add files via upload

--- ASM1/Fraction.java
+++ ASM1/Fraction.java
@@ -13,6 +13,7 @@ public class Fraction {
         this.tuSo = x;
         this.mauSo = y;
         daNhap = true;
+        nhapFraction(x, y);
     }
 
     public void nhapTu(int a) {
@@ -22,7 +23,6 @@ public class Fraction {
     public void nhapMau(int b) {
         if (b != 0) this.mauSo = b;
         else System.out.println("Mau so can khac 0.");
-        return;
     }
 
     public void nhapFraction(int a, int b) {
@@ -30,21 +30,22 @@ public class Fraction {
         nhapMau(b);
     }
 
+    //print
     public void inFraction() {
         if (!daNhap) System.out.println("Chua nhap phan so.");
         else System.out.print("Phan so hien tai la " + this.tuSo + "/" + this.mauSo);
     }
 
-    public void rutGonFraction() {
+    //rut gon
+    public Fraction rutGonFraction() {
         if (this.tuSo == 0) {
             this.mauSo = 1;
-            return;
+            Fraction temp = new Fraction(tuSo, mauSo);
+            temp.rutGonFraction();
+            return temp;
         }
         int ucln = 1;
-        int min;
-
-        if (Math.abs(this.tuSo) < Math.abs(this.mauSo)) min = Math.abs(this.tuSo);
-        else min = Math.abs(this.mauSo);
+        int min = Math.min(Math.abs(this.tuSo), Math.abs(this.mauSo));
 
         for (int i = 1; i <= min; i++) {
             if (this.tuSo % i == 0 && this.mauSo % i == 0) ucln = i;
@@ -55,13 +56,18 @@ public class Fraction {
             this.tuSo *= -1;
             this.mauSo *= -1;
         }
-        System.out.println("Phan so hien tai la " + this.tuSo + "/" + this.mauSo);
+        Fraction temp = new Fraction(tuSo, mauSo);
+        temp.rutGonFraction();
+        return temp;
     }
 
-    public void nghichDaoFraction() {
+    //nghich dao
+    public Fraction nghichDaoFraction() {
         if (this.tuSo == 0) {
             System.out.println("Khong the nghich dao phan so.");
-            return;
+            Fraction temp = new Fraction(this.tuSo, this.mauSo);
+            temp.rutGonFraction();
+            return temp;
         }
         int temp;
         temp = this.tuSo;
@@ -71,9 +77,12 @@ public class Fraction {
             this.tuSo *= -1;
             this.mauSo *= -1;
         }
-        System.out.println("Phan so hien tai la " + this.tuSo + "/" + this.mauSo);
+        Fraction temp2 = new Fraction(tuSo, mauSo);
+        temp2.rutGonFraction();
+        return temp2;
     }
 
+    //cong tru nhan chia
     public Fraction add(Fraction other) {
         int tuSoMoi;
         int mauSoMoi;
@@ -107,8 +116,8 @@ public class Fraction {
     public Fraction divine(Fraction other) {
         int tuSoMoi;
         int mauSoMoi;
-        tuSoMoi = this.tuSo * other.tuSo;
-        mauSoMoi = this.mauSo * other.mauSo;
+        tuSoMoi = this.tuSo * other.mauSo;
+        mauSoMoi = this.mauSo * other.tuSo;
         Fraction temp = new Fraction(tuSoMoi, mauSoMoi);
         temp.rutGonFraction();
         return temp;
